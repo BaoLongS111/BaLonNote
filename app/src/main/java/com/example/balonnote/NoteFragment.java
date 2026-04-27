816e398864682409c4dbc1ddcaa4626275b8d73b
balon <1138491931@qq.com>
2026-04-27 15:33:52 +0800

    layout: 创建Fragment和MenuItem
    创建了NoteFragment和TodoFragment，然后创建了NoteItem的动画布局，还没开始做动画

---
+++ app/src/main/java/com/example/balonnote/NoteFragment.java
@@ -0,0 +1,31 @@
+package com.example.balonnote;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class NoteFragment extends Fragment {
+
+    public static NoteFragment newInstance(String param1, String param2) {
+        NoteFragment fragment = new NoteFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_note, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/balonnote/TodoFragment.java
@@ -0,0 +1,32 @@
+package com.example.balonnote;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class TodoFragment extends Fragment {
+
+    public static TodoFragment newInstance(String param1, String param2) {
+        TodoFragment fragment = new TodoFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_todo, container, false);
+    }
+}
